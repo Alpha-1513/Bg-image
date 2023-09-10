ec1d1b480edb40bc54bae278fef596dd96d57e1a
Alpha-1513 <144524570+Alpha-1513@users.noreply.github.com>
2023-09-10 10:28:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ img = get_img_as_base64("image.jpg")
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
+background-image: url("https://www.pexels.com/photo/photo-of-skyline-at-night-3075993/");
 background-size: 180%;
 background-position: top left;
 background-repeat: no-repeat;
